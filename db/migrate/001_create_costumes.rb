37960c141c90eb3594ae1fe6308be0e5f3ac9a84
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-09 20:37:18 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.string :price
+      t.float :price
       t.string :size
       t.string :image_url
       t.timestamps
