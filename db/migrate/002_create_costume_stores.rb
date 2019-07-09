0ca9ef0de379ac1d26cc06d841e23ea34a7073e6
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-09 20:36:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,11 +4,11 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.string :costume_inventory
-      t.string :num_of_employees
-      t.string :still_in_business
-      t.string :opening_time
-      t.string :closing_time
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
