46fcb4b7f4cab64a6d441c425d4ac043f0721c94
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-09 20:29:10 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,9 +3,13 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name
+      t.string :location
+      t.string :theme
       t.string :price
-      t.string :size
-      t.string :image_url
+      t.boolean :family_friendly
+      t.string :opening_date
+      t.string :closing_date
+      t.string :description
     end
   end
 end
