160eb3e4003807bf6f87bb75ecca5dd50d5b346d
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-09 20:26:28 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,9 +3,12 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
-      t.string :price
-      t.string :size
-      t.string :image_url
+      t.string :location
+      t.string :costume_inventory
+      t.string :num_of_employees
+      t.string :still_in_business
+      t.string :opening_time
+      t.string :closing_time
     end
   end
 end
